0fc324bcd554f9385906e070e5a6365b94ab0bc1
Joke Heyndels <jo_hannah@outlook.com>
2019-08-09 14:38:11 +0200

    remove unnecessary h2 from template literal

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $.getJSON("data/motivational-quotes.json", function(json) {
     quotesData = json;
     
 document.getElementById("quote").innerHTML = `
-<h2>Quote of the Day</h2>
 <p class="motivational-quote">
 ${quotesData[Math.floor(Math.random() * quotesData.length)].quote}
 </p>
